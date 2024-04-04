2255a6ee86a71e70aa3c6626b6140327dc17467b
Nguyen The Anh <theanhnguyen@ssis.edu.vn>
2024-04-04 16:42:07 +0700

    update

---
+++ dim_product/sql/2_incremental_load_no_merge.sql
@@ -0,0 +1,41 @@
+-- Insert new records into dim_product_scd2
+INSERT INTO dim_product_scd2 (tiki_pid, name, brand_name, origin, ingestion_dt_unix, valid_from, valid_to, current_flag)
+SELECT
+    dim_product.tiki_pid,
+    dim_product.name,
+    dim_product.brand_name,
+    dim_product.origin,
+    dim_product.ingestion_dt_unix,
+    dim_product.ingestion_dt_unix,
+    NULL,
+    true
+FROM
+    dim_product
+LEFT JOIN
+    dim_product_scd2 ON dim_product.tiki_pid = dim_product_scd2.tiki_pid
+WHERE
+    dim_product_scd2.id IS NULL; -- Only insert if the record doesn't exist in dim_product_scd2
+
+
+
+
+-- Update existing records in dim_product_scd2
+UPDATE
+    dim_product_scd2
+SET
+    valid_to = sub.ingestion_dt_unix
+FROM (
+    SELECT
+        dim_product.tiki_pid,
+        dim_product_scd2.id,
+        dim_product.*
+    FROM
+        dim_product
+    JOIN
+        dim_product_scd2 ON dim_product.tiki_pid = dim_product_scd2.tiki_pid
+    WHERE
+        (dim_product.name <> dim_product_scd2.name OR dim_product.origin <> dim_product_scd2.origin OR dim_product.brand_name <> dim_product_scd2.brand_name)
+        AND dim_product_scd2.valid_to IS NULL
+) AS sub
+WHERE
+    dim_product_scd2.id = sub.id;
